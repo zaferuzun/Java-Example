816f69c45a7e424d88507ac70b2ea53d2abcf817
Zafer UZUN <uznzafer@gmail.com>
2021-10-21 14:54:59 +0300

    generic class Example

---
+++ genericClassExample/src/com/company/genericClass.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import com.sun.org.apache.xpath.internal.operations.String;
+
+import java.util.ArrayList;
+
+//<> içerisine yazılan şey farketmez lakin bu bir isimlendirme standartıdır.
+//Type'ı temsil eder
+public class genericClass <T> {
+    //ArrayList<String> genericlige örnektir.
+    //java tip güvenli bir dildir.
+    //new ArrayList<> içerisine type belirtilmesede oluyor lakin okunurluk bakımında önemlidir.
+    ArrayList<String> tipGuvenli =new ArrayList<String>();
+    //yaptıgımız işler aynıdır lakin verdigimiz nesne degişir.
+    //bu sebeple hepsine aynı işlemleri yapan classlar yazmak yerine
+    //bu şekilde generic classlar yazarız
+
+    //burda yapılan işlem ise T türünden bir obje ile işlem yapılacagını bildirir.
+    //T türü ne gönderilirse onun yerine geçecektir.
+    public void add(T object){
+
+    }
+}
